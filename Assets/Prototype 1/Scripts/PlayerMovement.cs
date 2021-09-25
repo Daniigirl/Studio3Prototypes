469f2c3ff9ce16e0f314a6b935751ed211d2c5ec
Daniela <1019475@student.sae.edu.au>
2021-09-25 15:17:43 +1000

    Applied force
    Foward and sideways force in script

--- Assets/Prototype 1/Scripts/PlayerMovement.cs
+++ Assets/Prototype 1/Scripts/PlayerMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 100f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("right") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
