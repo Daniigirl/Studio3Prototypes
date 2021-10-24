b13178d77b3436327298bd5dee7e637e995680f2
Daniela <1019475@student.sae.edu.au>
2021-10-24 21:17:03 +1100

    Added score and multiplier and more arrows
    song is almost up to the bridge

--- Assets/Prototype 5/Scripts/GameManager.cs
+++ Assets/Prototype 5/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,11 +14,25 @@ public class GameManager : MonoBehaviour
 
     public static GameManager instance;
 
+    public int currentScore;
+    public int scorePerNote = 50;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public Text scoreText;
+    public Text multiText;
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
         audioSource = GetComponent<AudioSource>();
+
+        scoreText.text = "Score: 0";
+
+        currentMultiplier = 1; 
     }
 
     // Update is called once per frame
@@ -39,10 +54,32 @@ public class GameManager : MonoBehaviour
     public void NoteHit()
     {
         Debug.Log("Hit on Time");
+
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
+
+            currentScore += scorePerNote * currentMultiplier;
+            scoreText.text = "Score: " + currentScore;
+        
     }
 
     public void NoteMissed()
     {
         Debug.Log("Missed Note");
+
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
     }
 }
