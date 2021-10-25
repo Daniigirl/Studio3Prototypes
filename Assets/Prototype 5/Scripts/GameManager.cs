cb9132b241b5c52724799e22f2c3d1023da5681d
Daniela <1019475@student.sae.edu.au>
2021-10-25 17:49:16 +1100

    Made particle effects and timing of hits

--- Assets/Prototype 5/Scripts/GameManager.cs
+++ Assets/Prototype 5/Scripts/GameManager.cs
@@ -16,6 +16,8 @@ public class GameManager : MonoBehaviour
 
     public int currentScore;
     public int scorePerNote = 50;
+    public int scorePerGoodNote = 75;
+    public int scorePerPerfectNote = 100;
 
     public int currentMultiplier;
     public int multiplierTracker;
@@ -68,9 +70,27 @@ public class GameManager : MonoBehaviour
 
         multiText.text = "Multiplier: x" + currentMultiplier;
 
-            currentScore += scorePerNote * currentMultiplier;
+            //currentScore += scorePerNote * currentMultiplier;
             scoreText.text = "Score: " + currentScore;
         
+    }
+
+    public void NormalHit()
+    {
+        currentScore += scorePerNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void GoodHit()
+    {
+        currentScore += scorePerGoodNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void PerfectHit()
+    {
+        currentScore += scorePerPerfectNote * currentMultiplier;
+        NoteHit();
     }
 
     public void NoteMissed()
