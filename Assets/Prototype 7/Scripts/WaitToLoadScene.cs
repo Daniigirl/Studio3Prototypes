60b1a4c2da21e4ecd9c505407f226839069547ea
Daniela <1019475@student.sae.edu.au>
2021-11-12 20:16:16 +1100

    Setting up button panels for menu and logo scene

---
+++ Assets/Prototype 7/Scripts/WaitToLoadScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaitToLoadScene : MonoBehaviour
+{
+    public string myScene;
+    public float wait_time = 6f;
+
+    private void Start()
+    {
+        StartCoroutine(Wait_for_intro());
+    }
+
+    IEnumerator Wait_for_intro()
+    {
+        yield return new WaitForSeconds(wait_time);
+
+        SceneManager.LoadScene(myScene);
+    }
+}
+
